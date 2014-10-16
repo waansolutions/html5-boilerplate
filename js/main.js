e9996f22063add671c1e703f4b36e87d37847a25
Anupa Perera <anup.perera@gmail.com>
2014-10-16 15:43:40 +0530

    -- jq idle timer added --

--- js/main.js
+++ js/main.js
@@ -6,4 +6,29 @@ var $j = jQuery.noConflict();
 $j(function(){
 
     $j('#jquery-txt').append("<br />Jquery is working");
+
+    $j.idleTimeout('#idletimeout','#idletimeout a',{
+
+        // uncomment and add server file if you need server respond
+        //keepAliveURL: 'server.php',
+        //serverResponseEquals: 'OK',
+
+        onTimeout: function(){
+
+            $j(this).slideUp();
+            window.location = "?action=logout";
+        },
+        onIdle: function(){
+
+            $j(this).slideDown();
+        },
+        onCountdown: function(counter){
+
+            $j(this).find("span").html(counter);
+        },
+        onResume: function(){
+
+            $j(this).slideUp();
+        }
+    });
 });
